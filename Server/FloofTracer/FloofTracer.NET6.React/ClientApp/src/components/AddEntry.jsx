5396195ea23c2a1616f0290241b3f0a8a568f968
Markus <5563643+MarkusGeiger@users.noreply.github.com>
2022-01-22 08:39:21 +0100

    Add time reset button, add presets for food size

--- Server/FloofTracer/FloofTracer.NET6.React/ClientApp/src/components/AddEntry.jsx
+++ Server/FloofTracer/FloofTracer.NET6.React/ClientApp/src/components/AddEntry.jsx
@@ -7,20 +7,28 @@ import {
   Row,
   Col,
   TimePicker,
+  Radio,
 } from 'antd';
 import dayjs from 'dayjs';
 
 export class AddEntry extends Component {
   static displayName = AddEntry.name;
+  static presetOptions = [
+    { label: "Custom", value: 0 },
+    { label: "1 EL", value: 1, presetFoodValue: 15 },
+    { label: "Klein", value: 2, presetFoodValue: 40 },
+    { label: "Groß", value: 3, presetFoodValue: 75 }
+  ]
 
   constructor(props) {
     super(props);
-    this.state = { value: 40, pets: [], submitDisabled: false, time: dayjs() };
+    this.state = { value: 40, pets: [], submitDisabled: false, time: dayjs(), loading: false, preset: 2 };
 
     this.handleAmountChange = this.handleAmountChange.bind(this);
     this.handleCatChange = this.handleCatChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleTimeChange = this.handleTimeChange.bind(this);
+    this.handleRadioChange = this.handleRadioChange.bind(this);
   }
 
   componentDidMount() {
@@ -62,7 +70,7 @@ export class AddEntry extends Component {
   }
 
   handleAmountChange(amount) {
-    this.setState({ value: amount, submitDisabled: false });
+    this.setState({ value: amount, submitDisabled: false, preset: 0 });
   }
 
   handleCatChange(event, name) {
@@ -75,6 +83,7 @@ export class AddEntry extends Component {
 
   async handleSubmit(event) {
     console.log('New submission. Amount: ', this.state.value, ', Pets: ', this.state.pets);
+    this.setState({ loading: true });
     //event.preventDefault();
     const selectedPets = this.state.pets.filter(pet => pet.selected);
     console.log("SelectedPets: ", selectedPets);
@@ -83,12 +92,29 @@ export class AddEntry extends Component {
       await this.postFood(selectedPets[selectedPet]);
     }
     console.log("Data submission done...calling parent");
+    this.setState({ loading: false });
     this.props.dataSubmitted();
   }
 
   handleTimeChange(time) {
     console.log("time changed: ", time);
     this.setState({ time: time });
+  }
+
+  handleRadioChange(event) {
+    console.log("Radio changed: ", event, "target value: ", event.target.value, "state: ", this.state);
+    if (event.target.value != null) {
+      console.log("DEBUG searchvalue", event.target.value, "preset: ", AddEntry.presetOptions);
+      const foundEntry = AddEntry.presetOptions.find(option => option.value === event.target.value);
+      if (foundEntry) {
+        let presetValue = this.state.value;
+        console.log("DEBUG", foundEntry);
+        if (foundEntry.presetFoodValue) {
+          presetValue = foundEntry.presetFoodValue;
+        }
+        this.setState({ preset: event.target.value, value: presetValue });
+      }
+    }
   }
 
   render() {
@@ -100,10 +126,18 @@ export class AddEntry extends Component {
           id="entryform"
           size="small">
           <Form.Item label="Uhrzeit">
-            <TimePicker value={this.state.time} onChange={this.handleTimeChange}/>
+            <TimePicker value={this.state.time} onChange={this.handleTimeChange} /><Button style={{ marginLeft: "5px" }} onClick={() => this.setState({time:dayjs()})}>Jetzt</Button>
           </Form.Item>
           <Form.Item label="Menge">
-            <InputNumber addonAfter="g" defaultValue={this.state.value} min={1} max={400} onChange={this.handleAmountChange} />
+            <InputNumber addonAfter="g" value={this.state.value} defaultValue={this.state.value} min={1} max={400} onChange={this.handleAmountChange} />
+          </Form.Item>
+          <Form.Item label="Voreinstellung">
+            <Radio.Group
+              options={AddEntry.presetOptions}
+              onChange={this.handleRadioChange}
+              value={this.state.preset}
+              optionType="button"
+            />
           </Form.Item>
           <Form.Item name="checkbox-group" label="Katzi">
               <Row>
@@ -116,7 +150,7 @@ export class AddEntry extends Component {
               </Row>
           </Form.Item>
           <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit" disabled={this.state.submitDisabled}>
+            <Button type="primary" htmlType="submit" loading={this.state.loading} disabled={this.state.submitDisabled || this.state.loading}>
               Hinzufügen
             </Button>
           </Form.Item>
